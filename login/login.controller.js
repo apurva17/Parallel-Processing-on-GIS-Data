a2f27a860be4014772bbabc3845365a5ba9f5f64
apurva17 <apurvak1@umbc.edu>
2018-09-25 23:57:59 -0400

    Adding project to Github

---
+++ login/login.controller.js
@@ -0,0 +1,77 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService','$http'];
+    function LoginController($location, AuthenticationService, FlashService,$http) {
+        var vm = this;
+
+        vm.login = login;
+
+        (function initController() {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+        })();
+
+        function login() {
+            vm.dataLoading = true;
+
+
+
+            var http = new XMLHttpRequest();
+            var params = "text=stuff";
+            http.open("POST", "http://localhost:3000", true);
+
+
+
+
+            AuthenticationService.Login(vm.username, vm.password, function (response) {
+                if (response.success) {
+                    AuthenticationService.SetCredentials(vm.username, vm.password);
+
+
+                    http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                    console.log("here for login");
+
+
+                   /* http.send( JSON.stringify({
+                        type: "authentication",
+                        username: vm.user.username,
+                        password:vm.user.password
+
+
+                    }));
+*/
+                    $location.path('/');
+                } else {
+                    FlashService.Error(response.message);
+                    vm.dataLoading = false;
+
+                }
+
+
+
+
+
+                http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                console.log("here for login");
+
+
+                http.send( JSON.stringify({
+                    type: "authentication",
+                    username: vm.user.username,
+                    password:vm.user.password
+
+
+                }));
+
+
+
+            });
+        };
+    }
+
+})();
---
+++ register/register.controller.js
@@ -0,0 +1,55 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('RegisterController', RegisterController);
+
+    RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService','$http'];
+    function RegisterController(UserService, $location, $rootScope, FlashService,$http) {
+        var vm = this;
+
+        vm.register = register;
+
+        function register() {
+            vm.dataLoading = true;
+            UserService.Create(vm.user)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Registration successful', true);
+                        $location.path('/login');
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+
+            var http = new XMLHttpRequest();
+            var params = "text=stuff";
+            http.open("POST", "http://localhost:3000", true);
+
+
+            http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+
+
+            http.send( JSON.stringify({
+                type: "Registeration",
+                firstname: vm.user.firstName,
+                lastname:vm.user.lastName,
+                username: vm.user.username,
+                password:vm.user.password,
+                Ethnicity:vm.user.Ethnicity,
+                location:vm.user.Location,
+                gender:vm.user.Gender
+
+
+
+            }));
+
+
+
+        }
+    }
+
+})();
